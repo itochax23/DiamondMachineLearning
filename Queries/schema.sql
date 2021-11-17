afda0a6139437b50495c2f14c857ec5c98ed5be5
Andy LaBounty <alabounty23@gmail.com>
2021-11-17 15:42:17 -0600

    CSV to dataframe code + df cleanup

---
+++ Queries/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE diamonds (
+	id INT NOT NULL,
+	url VARCHAR NOT NULL,
+	shape VARCHAR NOT NULL,
+	price INT NOT NULL,
+	carrot DECIMAL NOT NULL,
+	cut VARCHAR NOT NULL,
+	color VARCHAR NOT NULL,
+	clarity VARCHAR NOT NULL,
+	report VARCHAR NOT NULL,
+	type VARCHAR NOT NULL,
+	date_fetched VARCHAR NOT NULL
+);
+
+DROP TABLE diamonds;
+
+select * from diamonds;
