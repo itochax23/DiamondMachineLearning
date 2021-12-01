76a57a87b74a2c4c0eb936e8b9cd31c00887a73e
Sarah Volk <volkx075@d.umn.edu>
2021-12-01 13:13:03 -0600

    Update html file.

---
+++ static/js/scripts.js
@@ -0,0 +1,82 @@
+/*!
+* Start Bootstrap - Agency v7.0.10 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+  // Navbar shrink function
+  var navbarShrink = function () {
+      const navbarCollapsible = document.body.querySelector('#mainNav');
+      if (!navbarCollapsible) {
+          return;
+      }
+      if (window.scrollY === 0) {
+          navbarCollapsible.classList.remove('navbar-shrink')
+      } else {
+          navbarCollapsible.classList.add('navbar-shrink')
+      }
+
+  };
+
+  // Shrink the navbar 
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener('scroll', navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector('#mainNav');
+  if (mainNav) {
+      new bootstrap.ScrollSpy(document.body, {
+          target: '#mainNav',
+          offset: 74,
+      });
+  };
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+      document.querySelectorAll('#navbarResponsive .nav-link')
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+      responsiveNavItem.addEventListener('click', () => {
+          if (window.getComputedStyle(navbarToggler).display !== 'none') {
+              navbarToggler.click();
+          }
+      });
+  });
+
+
+function getData() {
+  const shape = document.getElementById("shape").value;
+  const cut = document.getElementById("cut").value;
+  const color = document.getElementById("color").value;
+  const clarity = document.getElementById("clarity").value;
+  const carat = +document.getElementById("carat").value;
+  const price = +document.getElementById("price").value;
+  const report = document.getElementById("report").value;
+
+  return {
+    shape,
+    cut,
+    color,
+    clarity,
+    carat,
+    price,
+    report,
+  };
+}
+
+async function submit() {
+  const response = await fetch('/predict', {
+    method: 'POST',
+    body: JSON.stringify(getData()),
+  });
+  const dataBack = await response.text();
+  alert(dataBack);
+}
