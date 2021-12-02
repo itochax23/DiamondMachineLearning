ed503db0c360a88aecb88fc20b859effda895702
Sarah Volk <volkx075@d.umn.edu>
2021-12-01 19:33:21 -0600

    Update website.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -9,74 +9,74 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-  // Navbar shrink function
-  var navbarShrink = function () {
-      const navbarCollapsible = document.body.querySelector('#mainNav');
-      if (!navbarCollapsible) {
-          return;
-      }
-      if (window.scrollY === 0) {
-          navbarCollapsible.classList.remove('navbar-shrink')
-      } else {
-          navbarCollapsible.classList.add('navbar-shrink')
-      }
+	// Navbar shrink function
+	var navbarShrink = function () {
+		const navbarCollapsible = document.body.querySelector('#mainNav');
+		if (!navbarCollapsible) {
+			return;
+		}
+		if (window.scrollY === 0) {
+			navbarCollapsible.classList.remove('navbar-shrink')
+		} else {
+			navbarCollapsible.classList.add('navbar-shrink')
+		}
 
-  };
+	};
 
-  // Shrink the navbar 
-  navbarShrink();
+	// Shrink the navbar 
+	navbarShrink();
 
-  // Shrink the navbar when page is scrolled
-  document.addEventListener('scroll', navbarShrink);
+	// Shrink the navbar when page is scrolled
+	document.addEventListener('scroll', navbarShrink);
 
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector('#mainNav');
-  if (mainNav) {
-      new bootstrap.ScrollSpy(document.body, {
-          target: '#mainNav',
-          offset: 74,
-      });
-  };
+	// Activate Bootstrap scrollspy on the main nav element
+	const mainNav = document.body.querySelector('#mainNav');
+	if (mainNav) {
+		new bootstrap.ScrollSpy(document.body, {
+			target: '#mainNav',
+			offset: 74,
+		});
+	};
 
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector('.navbar-toggler');
-  const responsiveNavItems = [].slice.call(
-      document.querySelectorAll('#navbarResponsive .nav-link')
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-      responsiveNavItem.addEventListener('click', () => {
-          if (window.getComputedStyle(navbarToggler).display !== 'none') {
-              navbarToggler.click();
-          }
-      });
-  });
-}
+	// Collapse responsive navbar when toggler is visible
+	const navbarToggler = document.body.querySelector('.navbar-toggler');
+	const responsiveNavItems = [].slice.call(
+		document.querySelectorAll('#navbarResponsive .nav-link')
+	);
+	responsiveNavItems.map(function (responsiveNavItem) {
+		responsiveNavItem.addEventListener('click', () => {
+			if (window.getComputedStyle(navbarToggler).display !== 'none') {
+				navbarToggler.click();
+			}
+		});
+	});
+});
 
 function getData() {
-  const shape = document.getElementById("shape").value;
-  const cut = document.getElementById("cut").value;
-  const color = document.getElementById("color").value;
-  const clarity = document.getElementById("clarity").value;
-  const carat = +document.getElementById("carat").value;
-  const price = +document.getElementById("price").value;
-  const report = document.getElementById("report").value;
+	const shape = document.getElementById("shape").value;
+	const cut = document.getElementById("cut").value;
+	const color = document.getElementById("color").value;
+	const clarity = document.getElementById("clarity").value;
+	const carat = +document.getElementById("carat").value;
+	const price = +document.getElementById("price").value;
+	const report = document.getElementById("report").value;
 
-  return {
-    shape,
-    cut,
-    color,
-    clarity,
-    carat,
-    price,
-    report,
-  };
+	return {
+		shape,
+		cut,
+		color,
+		clarity,
+		carat,
+		price,
+		report,
+	};
 }
 
 async function submit() {
-  const response = await fetch('/predict', {
-    method: 'POST',
-    body: JSON.stringify(getData()),
-  });
-  const dataBack = await response.text();
-  alert(dataBack);
+	const response = await fetch('/predict', {
+		method: 'POST',
+		body: JSON.stringify(getData()),
+	});
+	const dataBack = await response.text();
+	alert(dataBack);
 }
